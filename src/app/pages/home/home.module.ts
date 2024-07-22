800f8ff3099ef2e47500958a8614f87e576c0606
Mounita Bhandari <71941909+mounitbhandari@users.noreply.github.com>
2024-07-22 22:21:43 +0530

    Changes on home page
    Changes on home page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,6 +16,12 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
     
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
